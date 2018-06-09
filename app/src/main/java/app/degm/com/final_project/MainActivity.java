e19f5570c4ba73e8bf0ca0294591a845fec1b514
neox-hk <daga9420@gmail.com>
2018-06-09 10:18:59 -0500

    Initial commit

---
+++ app/src/main/java/app/degm/com/final_project/MainActivity.java
@@ -0,0 +1,13 @@
+package app.degm.com.final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+goo
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
